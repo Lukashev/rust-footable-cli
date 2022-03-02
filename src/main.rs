7e6ea5bf5efefce3c97851d6f0a74ffa8bc43454
nlukashev+1-at-801332968575 <lnv161297@gmail.com>
2022-03-02 09:50:38 +0200

    parse html and show table in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use curl::easy::Easy;
 use std::io::{stdin};
 use scraper::{Html, Selector};
+use prettytable::{Table, Row, Cell, cell, row};
 
 /// Get part of request url by provided league
 /// # Examples: 
@@ -43,14 +44,59 @@ fn request_data(league: i8) -> String {
     return html_data;
 }
 
-fn parse_html(html_string: String) {
+// Parse HTML document 
+// * `html_string` - html string
+fn parse_html(html_string: String) -> Vec<Vec<String>> {
     let doc_str: String = format!("{}", html_string);
     let document = Html::parse_document(&doc_str);
-    let row_selector = Selector::parse(r#"tr.standing-table__row"#).unwrap();
+    let row_selector: Selector = Selector::parse(r#"tr.standing-table__row"#).unwrap();
+
+    let mut result: Vec<Vec<String>> = Vec::new();
 
     for element in document.select(&row_selector) {
-        println!("{:?}", element);
+        let col_selector: Selector = Selector::parse("td.standing-table__cell").unwrap();
+        let columns: Vec<&str> = element
+            .select(&col_selector)
+            .flat_map(|x| x.text())
+            .collect::<Vec<&str>>();
+        
+        if columns.len() == 0 {
+            continue;
+        };
+        
+        let mut tmp_cols: Vec<String> = Vec::new();
+        let mut tmp_idx = 0;
+        let allow_row_idx: [u8; 7] = [0, 2, 4, 5, 6, 7, 11];
+
+        for col in &columns {
+            if allow_row_idx.contains(&tmp_idx) {
+                tmp_cols.push(col.to_string());
+            }
+            tmp_idx += 1;
+        }
+        
+        result.push(tmp_cols);
     }
+
+    return result
+}
+
+// Print table in terminal
+fn show_table(rows: Vec<Vec<String>>) {
+    let mut table = Table::new();
+
+    table.add_row(row!["#", "Team", "PL", "W", "D", "L", "Pts"]);
+
+    for row in &rows {
+        let table_cells = row
+            .into_iter()
+            .map(|c| Cell::new(c))
+            .collect();
+
+        table.add_row(Row::new(table_cells));
+    }
+
+    table.printstd();
 }
 
 fn main() {
@@ -74,14 +120,11 @@ fn main() {
 
         if !is_stopped {
             match buf.trim().parse::<i8>() {
-                Ok(n) => parse_html(request_data(n)),
+                Ok(n) => show_table(parse_html(request_data(n))),
                 Err(_) => {
                     println!("You entered wrong league code. Please, ")
                 }
             }
         }
-
     }
-
-
 }
